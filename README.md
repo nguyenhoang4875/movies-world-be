439f3303834dde7d1311287951be896975fa4db6
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-18 01:05:40 +0700

    add dependency security and hibernate

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>Movies's World</h1>
+<h2>Test project</h2>
+
+Clone and run project login with account:
+
+> username: teo <br>
+> password: te123 <br>
+
+After login ==> Whitelabel Error Page ==> done <br>
+(because of not yet implement login page)<br>
+When login with wrong account ==> Bad credentials <br>
+
---
+++ src/main/java/com/movies/config/MoviesAppConfig.java
@@ -0,0 +1,12 @@
+package com.movies.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.movies")
+public class MoviesAppConfig {
+
+}
