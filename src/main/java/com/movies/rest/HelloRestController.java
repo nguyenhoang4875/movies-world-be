d454e417994b3e1f5bc008e2b1d56b6112bf411f
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-22 09:50:47 +0700

    add rest api HelloRestController

---
+++ src/main/java/com/movies/rest/HelloRestController.java
@@ -0,0 +1,15 @@
+package com.movies.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/")
+    public String sayHello(){
+       return "Hello world! time on server is " + LocalDateTime.now();
+    }
+}
