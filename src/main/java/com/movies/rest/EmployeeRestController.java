48e3eb605ed2dbb07df29b4de8d55b486068c3ba
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-24 17:29:04 +0700

    complete crud Employee

--- src/main/java/com/movies/rest/EmployeeRestController.java
+++ src/main/java/com/movies/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.movies.rest;
 import com.movies.entity.Employee;
 import com.movies.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,58 @@ public class EmployeeRestController {
     public List<Employee> findAll() {
         employeeService.findAll().forEach(System.out::println);
         return employeeService.findAll();
+    }
+
+    // add mapping for GET /employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    // add mapping for POST /employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+    // add mapping for DELETE /employees - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee employee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Delete employee id - " + employeeId;
     }
 
 }
