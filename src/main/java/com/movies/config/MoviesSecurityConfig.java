8e840682953956069663ddbb9b654be5c3df41a9
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-24 17:29:45 +0700

    disable csrf spring security

--- src/main/java/com/movies/config/MoviesSecurityConfig.java
+++ src/main/java/com/movies/config/MoviesSecurityConfig.java
@@ -2,6 +2,7 @@ package com.movies.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,4 +15,8 @@ public class MoviesSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}teo123").roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 }
