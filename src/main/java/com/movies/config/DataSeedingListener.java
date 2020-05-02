d2defc159e07cc5f2951d3f0598e001c79938985
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-05-03 00:07:34 +0700

    login return jwt v1.0

---
+++ src/main/java/com/movies/config/DataSeedingListener.java
@@ -0,0 +1,71 @@
+package com.movies.config;
+
+
+import com.movies.entity.Role;
+import com.movies.entity.User;
+import com.movies.repository.RoleRepository;
+import com.movies.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+
+@Component
+@Configuration
+public class DataSeedingListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Value("${jwt-key}")
+    private String signingKey;
+
+
+    private void addRoleIfMissing(String name, String description) {
+        if (roleRepository.findByName(name) == null) {
+            roleRepository.save(new Role(name, description));
+        }
+    }
+
+    private void addUserIfMissing(String username, String password, String... roles) {
+        if (userRepository.findByUsername(username) == null) {
+            User user = new User(username, new BCryptPasswordEncoder().encode(password), "John Doe");
+            user.setRoles(new HashSet<>());
+
+            for (String role : roles) {
+                user.getRoles().add(roleRepository.findByName(role));
+            }
+
+            userRepository.save(user);
+        }
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+
+        addRoleIfMissing("ROLE_ADMIN", "Administrators");
+        addRoleIfMissing("ROLE_STAFF", "Staffs");
+
+        addUserIfMissing("user", "fun123", "ROLE_STAFF");
+        addUserIfMissing("admin", "fun123", "ROLE_STAFF", "ROLE_ADMIN");
+
+        if (signingKey == null || signingKey.length() == 0) {
+            String jws = Jwts.builder()
+                    .setSubject("MoviesWorld")
+                    .signWith(SignatureAlgorithm.HS256, "MoviesWorld").compact();
+
+            System.out.println("Use this jwt key:");
+            System.out.println("jwt-key=" + jws);
+        }
+    }
+}
--- src/main/java/com/movies/controlller/AuthController.java
+++ src/main/java/com/movies/controlller/AuthController.java
@@ -1,18 +1,29 @@
 package com.movies.controlller;
 
+import com.movies.config.TokenProvider;
 import com.movies.entity.ConfirmationToken;
+import com.movies.entity.Role;
 import com.movies.entity.User;
+import com.movies.entity.dto.AuthToken;
+import com.movies.entity.dto.Login;
 import com.movies.service.ConfirmationTokenService;
 import com.movies.service.RoleService;
 import com.movies.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
@@ -29,19 +40,26 @@ public class AuthController {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
 
     @PostMapping("/register")
     public String registerAccount(@RequestBody User user, HttpServletRequest request) {
         User existingUser = userService.findOneByUsername((user.getUsername()));
         String message = "";
-        if (existingUser != null){
+        if (existingUser != null) {
             message = "This user already exists!";
-        }
-        else{
+        } else {
             BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
             user.setEnable(false);
             user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-            user.setRole(roleService.findOneByName("Customer"));
+            Set<Role> tempRoles = new HashSet<>();
+            tempRoles.add(roleService.findOneByName("ROLE_CUSTOMER"));
+            user.setRoles(tempRoles);
             userService.save(user);
             ConfirmationToken confirmationToken = new ConfirmationToken(user);
             confirmationTokenService.save(confirmationToken);
@@ -52,20 +70,20 @@ public class AuthController {
             mailMessage.setText("To confirm your account, please click here : "
                     + "http://localhost:9000/api/confirm-account?token=" + confirmationToken.getToken());
             javaMailSender.send(mailMessage);
-            message ="Successful Registration!";
+            message = "Successful Registration!";
         }
         return message;
     }
+
     @GetMapping("/confirm-account")
-    public String confirmRegister(@RequestParam("token") String confirmationToken){
+    public String confirmRegister(@RequestParam("token") String confirmationToken) {
         ConfirmationToken token = confirmationTokenService.findByToken(confirmationToken);
-        if (token != null){
+        if (token != null) {
             User user = userService.findOneByUsername(token.getUser().getUsername());
             user.setEnable(true);
             userService.save(user);
             return "Account verified!";
-        }
-        else{
+        } else {
             return "The link is invalid or broken!";
         }
     }
@@ -74,4 +92,19 @@ public class AuthController {
     public User getProfile(Principal principal) {
         return userService.findOneByUsername(principal.getName());
     }
+
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> login(@RequestBody Login login) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        login.getUsername(),
+                        login.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new AuthToken(token));
+    }
+
 }
