3d570193336fb7e0b56faef4207885f28dcc6744
cindy197198 <trangnguyen197198@gmail.com>
2020-05-20 23:31:07 +0700

    remove import

--- src/main/java/com/movies/controlller/AuthController.java
+++ src/main/java/com/movies/controlller/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
