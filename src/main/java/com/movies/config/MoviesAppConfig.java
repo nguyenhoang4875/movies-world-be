0a727e57f85fdf105428e5dd7e001ced26455c40
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-05-20 23:07:38 +0700

    remove bean Encoder in MoviesAppConfig

--- src/main/java/com/movies/config/MoviesAppConfig.java
+++ src/main/java/com/movies/config/MoviesAppConfig.java
@@ -1,23 +1,16 @@
 package com.movies.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackages = "com.movies")
 public class MoviesAppConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(11);
     }
 }
