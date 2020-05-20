ef7fdae6c8019f579539cbeb560af6657391e109
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-05-20 21:01:11 +0700

    add resolve exceptions

---
+++ src/main/java/com/movies/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.movies.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
