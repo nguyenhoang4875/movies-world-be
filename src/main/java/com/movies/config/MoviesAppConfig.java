3ddd2d43c14ce2e70c9ddf5d36cc32526e51d2d6
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-05-20 20:47:13 +0700

    config global for CORS

--- src/main/java/com/movies/config/MoviesAppConfig.java
+++ src/main/java/com/movies/config/MoviesAppConfig.java
@@ -1,12 +1,13 @@
 package com.movies.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "com.movies")
-public class MoviesAppConfig {
-
+public class MoviesAppConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
