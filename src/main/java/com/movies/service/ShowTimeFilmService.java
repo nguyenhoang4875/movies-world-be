b958e96ee2b0bd23c6553b061b7477de417c039b
cindy197198 <trangnguyen197198@gmail.com>
2020-06-18 18:43:26 +0700

    return show time of film

--- src/main/java/com/movies/service/ShowTimeFilmService.java
+++ src/main/java/com/movies/service/ShowTimeFilmService.java
@@ -1,8 +1,10 @@
 package com.movies.service;
 
 import com.movies.entity.dao.ShowTimeFilm;
+import com.movies.entity.dto.FilmTimeDTO;
 import com.movies.entity.dto.ShowTimeFilmDto;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ShowTimeFilmService {
@@ -10,4 +12,12 @@ public interface ShowTimeFilmService {
     List<ShowTimeFilm> getShowTimeFilmByFilmId(Integer filmId);
 
     ShowTimeFilmDto addShowTimeFilm(Integer filmId, ShowTimeFilmDto showTimeFilmDto);
+
+    List<Date> getDateShow(Integer filmId);
+
+    List<Date> getTimeShow(Integer filmId, Date date);
+
+    List<ShowTimeFilm> getAll();
+
+    List<FilmTimeDTO> getShowTimeInDay(Date d);
 }
--- src/main/java/com/movies/service/impl/ShowTimeFilmServiceImpl.java
+++ src/main/java/com/movies/service/impl/ShowTimeFilmServiceImpl.java
@@ -1,7 +1,10 @@
 package com.movies.service.impl;
 
+import com.movies.converter.bases.Converter;
 import com.movies.entity.dao.Film;
 import com.movies.entity.dao.ShowTimeFilm;
+import com.movies.entity.dto.FilmDTO;
+import com.movies.entity.dto.FilmTimeDTO;
 import com.movies.entity.dto.ShowTimeFilmDto;
 import com.movies.repository.FilmRepository;
 import com.movies.repository.RoomRepository;
@@ -10,6 +13,9 @@ import com.movies.service.ShowTimeFilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,6 +29,9 @@ public class ShowTimeFilmServiceImpl implements ShowTimeFilmService {
 
     @Autowired
     private RoomRepository roomRepository;
+
+    @Autowired
+    private Converter<Film, FilmDTO> filmFilmDTOConverter;
 
     @Override
     public List<ShowTimeFilm> getShowTimeFilmByFilmId(Integer filmId) {
@@ -43,5 +52,39 @@ public class ShowTimeFilmServiceImpl implements ShowTimeFilmService {
         return showTimeFilmDto;
     }
 
+    @Override
+    public List<Date> getDateShow(Integer filmId) {
+        return showTimeFilmRepository.findDate(filmId);
+    }
+
+    @Override
+    public List<Date> getTimeShow(Integer filmId, Date date) {
+        return showTimeFilmRepository.findTime(filmId, date);
+    }
+
+    @Override
+    public List<ShowTimeFilm> getAll() {
+        return showTimeFilmRepository.findAllByTime();
+    }
+
+    @Override
+    public List<FilmTimeDTO> getShowTimeInDay(Date d) {
+        List<Film> films = showTimeFilmRepository.findFilmByDate(d);
+        List<FilmTimeDTO> filmTimeDTOS = new ArrayList<>();
+        for (Film film : films) {
+            FilmTimeDTO filmTimeDTO = new FilmTimeDTO();
+            filmTimeDTO.setFilm(filmFilmDTOConverter.convert(film));
+            List<Date> timeList = showTimeFilmRepository.findTimeFromNow(film.getId(), d);
+            List<String> stringList = new ArrayList<>();
+            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+            for (Date time: timeList) {
+                stringList.add(format.format(time));
+            }
+            filmTimeDTO.setTime(stringList);
+            filmTimeDTOS.add(filmTimeDTO);
+        }
+
+        return filmTimeDTOS;
+    }
 
 }
