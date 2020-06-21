f22e6fd411cba0d73394fcdb09a5469fd6054284
cindy197198 <trangnguyen197198@gmail.com>
2020-06-21 12:55:00 +0700

    add reservation update

---
+++ src/main/java/com/movies/entity/dto/DateTimeFilmDTO.java
@@ -0,0 +1,21 @@
+package com.movies.entity.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class DateTimeFilmDTO {
+    @JsonFormat(pattern = "E, dd-MM-yyyy")
+    private LocalDate date;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private List<LocalDateTime> timeList;
+
+    public DateTimeFilmDTO(LocalDate date) {
+        this.date = date;
+    }
+}
